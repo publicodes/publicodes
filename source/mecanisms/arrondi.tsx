tchak/mon-entreprise

:sparkles: transforme applicable si et non applicable si en mécanisme chainée
Par la même occasion, uniformise l'écriture des mécanismes chainées

publicodes/source/mecanisms/arrondi.tsx
@@ -1,37 +1,20 @@
-import { has } from 'ramda'
 import React from 'react'
-import { Trans } from 'react-i18next'
 import { InfixMecanism } from '../components/mecanisms/common'
-import { defaultNode, evaluateNode, mergeAllMissing } from '../evaluation'
-import { simplifyNodeUnit } from '../nodeUnits'
-import { mapTemporal, pureTemporal, temporalAverage } from '../temporal'
-import { EvaluatedNode, EvaluatedRule } from '../types'
-import { serializeUnit } from '../units'
-
-type MecanismRoundProps = {
-	explanation: ArrondiExplanation
-}
+import { evaluateNode, makeJsx, mergeMissing } from '../evaluation'
+import { EvaluatedNode } from '../types'
 
 export type ArrondiExplanation = {
-	value: EvaluatedNode<string, number>
-	decimals: EvaluatedNode<string, number>
+	valeur: EvaluatedNode<string, number>
+	arrondi: EvaluatedNode<string, number>
 }
-
-function MecanismRound({ explanation }: MecanismRoundProps) {
+	
+function MecanismArrondi({ explanation }) {
 	return (
-		<InfixMecanism value={explanation.value}>
-			{explanation.decimals.nodeValue !== false &&
-				explanation.decimals.isDefault != false && (
-					<p>
-						<Trans
-							i18nKey="arrondi-to-decimals"
-							count={explanation.decimals.nodeValue ?? undefined}
-						>
-							<strong>Arrondi à : </strong>
-							{{ count: explanation.decimals.nodeValue }} décimales
-						</Trans>
-					</p>
-				)}
+		<InfixMecanism value={explanation.valeur}>
+			<p>
+				<strong>Arrondi : </strong>
+				{makeJsx(explanation.arrondi)}
+			</p>
 		</InfixMecanism>
 	)
 }
@@ -40,66 +23,48 @@ function roundWithPrecision(n: number, fractionDigits: number) {
 	return +n.toFixed(fractionDigits)
 }
 
-function evaluate<Names extends string>(
-	cache,
-	situation,
-	parsedRules,
-	node: EvaluatedRule<Names, ArrondiExplanation>
-) {
+const evaluate = (cache, situation, parsedRules, node) => {
 	const evaluateAttribute = evaluateNode.bind(
 		null,
 		cache,
 		situation,
 		parsedRules
 	)
-	const value = simplifyNodeUnit(evaluateAttribute(node.explanation.value))
-	const decimals = evaluateAttribute(node.explanation.decimals)
+	const valeur = evaluateAttribute(node.explanation.valeur)
+	const nodeValue = valeur.nodeValue
+	let arrondi = node.explanation.arrondi
+	if (nodeValue !== false) {
+		arrondi = evaluateAttribute(arrondi)
+	}
 
-	const temporalValue = mapTemporal(
-		(val: number | false | null) =>
-			typeof val === 'number'
-				? roundWithPrecision(val, decimals.nodeValue)
-				: val,
-		value.temporalValue ?? pureTemporal(value.nodeValue)
-	)
-
-	const nodeValue = temporalAverage(temporalValue, value.unit)
 	return {
-		...node,
-		unit: value.unit,
-		nodeValue,
-		...(temporalValue.length > 1 && { temporalValue }),
-		missingVariables: mergeAllMissing([value, decimals]),
-		explanation: { value, decimals }
+		nodeValue:
+			typeof arrondi.nodeValue === 'number'
+				? roundWithPrecision(valeur.nodeValue, arrondi.nodeValue)
+				: arrondi.nodeValue === true
+				? roundWithPrecision(valeur.nodeValue, 0)
+				: arrondi.nodeValue === null
+				? null
+				: valeur.nodeValue,
+		explanation: { valeur, arrondi },
+		missingVariables: mergeMissing(valeur, arrondi)
 	}
 }
 
-export default (recurse, v) => {
+export default function Arrondi(recurse, v)  {
 	const explanation = {
-		value: has('valeur', v) ? recurse(v['valeur']) : recurse(v),
-		decimals: has('décimales', v) ? recurse(v['décimales']) : defaultNode(0)
-	} as ArrondiExplanation
-
+		valeur: recurse(v.valeur),
+		arrondi: recurse(v.arrondi)
+	}
 	return {
-		explanation,
 		evaluate,
-		jsx: MecanismRound,
+		jsx: MecanismArrondi,
+		explanation,
 		category: 'mecanism',
 		name: 'arrondi',
 		type: 'numeric',
-		unit: explanation.value.unit
+		unit: explanation.valeur.unit
 	}
 }
 
-export function unchainRoundMecanism(recurse, rawNode) {
-	const { arrondi, ...valeur } = rawNode
-	const arrondiValue = recurse(arrondi)
-
-	if (serializeUnit(arrondiValue.unit) === 'décimales') {
-		return { arrondi: { valeur, décimales: arrondiValue.nodeValue } }
-	} else if (arrondiValue.nodeValue === true) {
-		return { arrondi: { valeur } }
-	} else {
-		return valeur
-	}
-}
+Arrondi.nom = 'arrondi'

publicodes/source/mecanisms/plafond.tsx
@@ -1,0 +1,80 @@
+import React from 'react'
+import { InfixMecanism } from '../components/mecanisms/common'
+import { typeWarning } from '../error'
+import { evaluateNode, makeJsx, mergeMissing } from '../evaluation'
+import { convertNodeToUnit } from '../nodeUnits'
+
+function MecanismPlafond({ explanation }) {
+	return (
+		<InfixMecanism value={explanation.valeur}>
+			<p
+				style={
+					explanation.plafond.isActive
+						? { background: 'var(--lighterColor)', fontWeight: 'bold' }
+						: {}
+				}
+			>
+				<strong>Plafonné à : </strong>
+				{makeJsx(explanation.plafond)}
+			</p>
+		</InfixMecanism>
+	)
+}
+
+const evaluate = (cache, situation, parsedRules, node) => {
+	const evaluateAttribute = evaluateNode.bind(
+		null,
+		cache,
+		situation,
+		parsedRules
+	)
+	const valeur = evaluateAttribute(node.explanation.valeur)
+	let nodeValue = valeur.nodeValue
+	let plafond = node.explanation.plafond
+	if (nodeValue !== false) {
+		plafond = evaluateAttribute(plafond)
+		if (valeur.unit) {
+			try {
+				plafond = convertNodeToUnit(valeur.unit, plafond)
+			} catch (e) {
+				typeWarning(
+					cache._meta.contextRule,
+					"L'unité du plafond n'est pas compatible avec celle de la valeur à encadrer",
+					e
+				)
+			}
+		}
+	}
+	if (
+		typeof nodeValue === 'number' &&
+		typeof plafond.nodeValue === 'number' &&
+		nodeValue > plafond.nodeValue
+	) {
+		nodeValue = plafond.nodeValue
+		plafond.isActive = true
+	}
+	return {
+		nodeValue,
+		unit: valeur.unit,
+		explanation: { valeur, plafond },
+		missingVariables: mergeMissing(valeur, plafond)
+	}
+}
+
+export default function Plafond (recurse, v) {
+	const explanation = {
+		valeur: recurse(v.valeur),
+		plafond: recurse(v.plafond)
+	}
+	return {
+		evaluate,
+		jsx: MecanismPlafond,
+		explanation,
+		category: 'mecanism',
+		name: 'plafond',
+		type: 'numeric',
+		unit: explanation.valeur.unit
+	}
+}
+
+Plafond.nom = 'plafond'

publicodes/source/mecanisms/plancher.tsx
@@ -1,0 +1,79 @@
+import React from 'react'
+import { InfixMecanism } from '../components/mecanisms/common'
+import { typeWarning } from '../error'
+import { evaluateNode, makeJsx, mergeMissing } from '../evaluation'
+import { convertNodeToUnit } from '../nodeUnits'
+
+function MecanismPlancher({ explanation }) {
+	return (
+		<InfixMecanism value={explanation.valeur}>
+			<p
+				style={
+					explanation.plancher.isActive
+						? { background: 'var(--lighterColor)', fontWeight: 'bold' }
+						: {}
+				}
+			>
+				<strong>Plafonné à : </strong>
+				{makeJsx(explanation.plancher)}
+			</p>
+		</InfixMecanism>
+	)
+}
+
+const evaluate = (cache, situation, parsedRules, node) => {
+	const evaluateAttribute = evaluateNode.bind(
+		null,
+		cache,
+		situation,
+		parsedRules
+	)
+	const valeur = evaluateAttribute(node.explanation.valeur)
+	let nodeValue = valeur.nodeValue
+	let plancher = node.explanation.plancher
+	if (nodeValue !== false) {
+		plancher = evaluateAttribute(plancher)
+		if (valeur.unit) {
+			try {
+				plancher = convertNodeToUnit(valeur.unit, plancher)
+			} catch (e) {
+				typeWarning(
+					cache._meta.contextRule,
+					"L'unité du plancher n'est pas compatible avec celle de la valeur à encadrer",
+					e
+				)
+			}
+		}
+	}
+	if (
+		typeof nodeValue === 'number' &&
+		typeof plancher.nodeValue === 'number' &&
+		nodeValue < plancher.nodeValue
+	) {
+		nodeValue = plancher.nodeValue
+		plancher.isActive = true
+	}
+	return {
+		nodeValue,
+		explanation: { valeur, plancher },
+		missingVariables: mergeMissing(valeur, plancher)
+	}
+}
+
+export default function Plancher(recurse, v) {
+	const explanation = {
+		valeur: recurse(v.valeur),
+		plancher: recurse(v.plancher)
+	}
+	return {
+		evaluate,
+		jsx: MecanismPlancher,
+		explanation,
+		category: 'mecanism',
+		name: 'plancher',
+		type: 'numeric',
+		unit: explanation.valeur.unit
+	}
+}
+
+Plancher.nom = 'plancher'

publicodes/publicodes

source/mecanisms/arrondi.tsx
@@ -1,37 +1,20 @@
-import { has } from 'ramda'
 import React from 'react'
-import { Trans } from 'react-i18next'
 import { InfixMecanism } from '../components/mecanisms/common'
-import { defaultNode, evaluateNode, mergeAllMissing } from '../evaluation'
-import { simplifyNodeUnit } from '../nodeUnits'
-import { mapTemporal, pureTemporal, temporalAverage } from '../temporal'
-import { EvaluatedNode, EvaluatedRule } from '../types'
-import { serializeUnit } from '../units'
-
-type MecanismRoundProps = {
-	explanation: ArrondiExplanation
-}
+import { evaluateNode, makeJsx, mergeAllMissing } from '../evaluation'
+import { EvaluatedNode } from '../types'
 
 export type ArrondiExplanation = {
-	value: EvaluatedNode<string, number>
-	decimals: EvaluatedNode<string, number>
+	valeur: EvaluatedNode<string, number>
+	arrondi: EvaluatedNode<string, number>
 }
 
-function MecanismRound({ explanation }: MecanismRoundProps) {
+function MecanismArrondi({ explanation }) {
 	return (
-		<InfixMecanism value={explanation.value}>
-			{explanation.decimals.nodeValue !== false &&
-				explanation.decimals.isDefault != false && (
-					<p>
-						<Trans
-							i18nKey="arrondi-to-decimals"
-							count={explanation.decimals.nodeValue ?? undefined}
-						>
-							<strong>Arrondi à : </strong>
-							{{ count: explanation.decimals.nodeValue }} décimales
-						</Trans>
-					</p>
-				)}
+		<InfixMecanism value={explanation.valeur}>
+			<p>
+				<strong>Arrondi : </strong>
+				{makeJsx(explanation.arrondi)}
+			</p>
 		</InfixMecanism>
 	)
 }
@@ -40,66 +23,52 @@ function roundWithPrecision(n: number, fractionDigits: number) {
 	return +n.toFixed(fractionDigits)
 }
 
-function evaluate<Names extends string>(
-	cache,
-	situation,
-	parsedRules,
-	node: EvaluatedRule<Names, ArrondiExplanation>
-) {
+const evaluate = (cache, situation, parsedRules, node) => {
 	const evaluateAttribute = evaluateNode.bind(
 		null,
 		cache,
 		situation,
 		parsedRules
 	)
-	const value = simplifyNodeUnit(evaluateAttribute(node.explanation.value))
-	const decimals = evaluateAttribute(node.explanation.decimals)
+	const valeur = evaluateAttribute(node.explanation.valeur)
+	const nodeValue = valeur.nodeValue
+	let arrondi = node.explanation.arrondi
+	if (nodeValue !== false) {
+		arrondi = evaluateAttribute(arrondi)
+	}
 
-	const temporalValue = mapTemporal(
-		(val: number | false | null) =>
-			typeof val === 'number'
-				? roundWithPrecision(val, decimals.nodeValue)
-				: val,
-		value.temporalValue ?? pureTemporal(value.nodeValue)
-	)
-
-	const nodeValue = temporalAverage(temporalValue, value.unit)
 	return {
 		...node,
-		unit: value.unit,
-		nodeValue,
-		...(temporalValue.length > 1 && { temporalValue }),
-		missingVariables: mergeAllMissing([value, decimals]),
-		explanation: { value, decimals }
+		nodeValue:
+			typeof valeur.nodeValue !== 'number'
+				? valeur.nodeValue
+				: typeof arrondi.nodeValue === 'number'
+				? roundWithPrecision(valeur.nodeValue, arrondi.nodeValue)
+				: arrondi.nodeValue === true
+				? roundWithPrecision(valeur.nodeValue, 0)
+				: arrondi.nodeValue === null
+				? null
+				: valeur.nodeValue,
+		explanation: { valeur, arrondi },
+		missingVariables: mergeAllMissing([valeur, arrondi]),
+		unit: valeur.unit
 	}
 }
 
-export default (recurse, v) => {
+export default function Arrondi(recurse, v) {
 	const explanation = {
-		value: has('valeur', v) ? recurse(v['valeur']) : recurse(v),
-		decimals: has('décimales', v) ? recurse(v['décimales']) : defaultNode(0)
-	} as ArrondiExplanation
-
+		valeur: recurse(v.valeur),
+		arrondi: recurse(v.arrondi)
+	}
 	return {
-		explanation,
 		evaluate,
-		jsx: MecanismRound,
+		jsx: MecanismArrondi,
+		explanation,
 		category: 'mecanism',
 		name: 'arrondi',
 		type: 'numeric',
-		unit: explanation.value.unit
+		unit: explanation.valeur.unit
 	}
 }
 
-export function unchainRoundMecanism(recurse, rawNode) {
-	const { arrondi, ...valeur } = rawNode
-	const arrondiValue = recurse(arrondi)
-
-	if (serializeUnit(arrondiValue.unit) === 'décimales') {
-		return { arrondi: { valeur, décimales: arrondiValue.nodeValue } }
-	} else if (arrondiValue.nodeValue === true) {
-		return { arrondi: { valeur } }
-	} else {
-		return valeur
-	}
-}
+Arrondi.nom = 'arrondi'

source/mecanisms/plafond.tsx
@@ -1,0 +1,82 @@
+import React from 'react'
+import { InfixMecanism } from '../components/mecanisms/common'
+import { typeWarning } from '../error'
+import { evaluateNode, makeJsx, mergeAllMissing } from '../evaluation'
+import { convertNodeToUnit } from '../nodeUnits'
+
+function MecanismPlafond({ explanation }) {
+	return (
+		<InfixMecanism value={explanation.valeur}>
+			<p
+				style={
+					explanation.plafond.isActive
+						? { background: 'var(--lighterColor)', fontWeight: 'bold' }
+						: {}
+				}
+			>
+				<strong>Plafonné à : </strong>
+				{makeJsx(explanation.plafond)}
+			</p>
+		</InfixMecanism>
+	)
+}
+
+const evaluate = (cache, situation, parsedRules, node) => {
+	const evaluateAttribute = evaluateNode.bind(
+		null,
+		cache,
+		situation,
+		parsedRules
+	)
+	const valeur = evaluateAttribute(node.explanation.valeur)
+
+	let nodeValue = valeur.nodeValue
+	let plafond = node.explanation.plafond
+	if (nodeValue !== false) {
+		plafond = evaluateAttribute(plafond)
+		if (valeur.unit) {
+			try {
+				plafond = convertNodeToUnit(valeur.unit, plafond)
+			} catch (e) {
+				typeWarning(
+					cache._meta.contextRule,
+					"L'unité du plafond n'est pas compatible avec celle de la valeur à encadrer",
+					e
+				)
+			}
+		}
+	}
+	if (
+		typeof nodeValue === 'number' &&
+		typeof plafond.nodeValue === 'number' &&
+		nodeValue > plafond.nodeValue
+	) {
+		nodeValue = plafond.nodeValue
+		plafond.isActive = true
+	}
+	return {
+		...node,
+		nodeValue,
+		unit: valeur.unit,
+		explanation: { valeur, plafond },
+		missingVariables: mergeAllMissing([valeur, plafond])
+	}
+}
+
+export default function Plafond(recurse, v) {
+	const explanation = {
+		valeur: recurse(v.valeur),
+		plafond: recurse(v.plafond)
+	}
+	return {
+		evaluate,
+		jsx: MecanismPlafond,
+		explanation,
+		category: 'mecanism',
+		name: 'plafond',
+		type: 'numeric',
+		unit: explanation.valeur.unit
+	}
+}
+
+Plafond.nom = 'plafond'

source/mecanisms/plancher.tsx
@@ -1,0 +1,81 @@
+import React from 'react'
+import { InfixMecanism } from '../components/mecanisms/common'
+import { typeWarning } from '../error'
+import { evaluateNode, makeJsx, mergeAllMissing } from '../evaluation'
+import { convertNodeToUnit } from '../nodeUnits'
+
+function MecanismPlancher({ explanation }) {
+	return (
+		<InfixMecanism value={explanation.valeur}>
+			<p
+				style={
+					explanation.plancher.isActive
+						? { background: 'var(--lighterColor)', fontWeight: 'bold' }
+						: {}
+				}
+			>
+				<strong>Minimum : </strong>
+				{makeJsx(explanation.plancher)}
+			</p>
+		</InfixMecanism>
+	)
+}
+
+const evaluate = (cache, situation, parsedRules, node) => {
+	const evaluateAttribute = evaluateNode.bind(
+		null,
+		cache,
+		situation,
+		parsedRules
+	)
+	const valeur = evaluateAttribute(node.explanation.valeur)
+	let nodeValue = valeur.nodeValue
+	let plancher = node.explanation.plancher
+	if (nodeValue !== false) {
+		plancher = evaluateAttribute(plancher)
+		if (valeur.unit) {
+			try {
+				plancher = convertNodeToUnit(valeur.unit, plancher)
+			} catch (e) {
+				typeWarning(
+					cache._meta.contextRule,
+					"L'unité du plancher n'est pas compatible avec celle de la valeur à encadrer",
+					e
+				)
+			}
+		}
+	}
+	if (
+		typeof nodeValue === 'number' &&
+		typeof plancher.nodeValue === 'number' &&
+		nodeValue < plancher.nodeValue
+	) {
+		nodeValue = plancher.nodeValue
+		plancher.isActive = true
+	}
+	return {
+		...node,
+		nodeValue,
+		explanation: { valeur, plancher },
+		missingVariables: mergeAllMissing([valeur, plancher]),
+		unit: valeur.unit
+	}
+}
+
+export default function Plancher(recurse, v) {
+	const explanation = {
+		valeur: recurse(v.valeur),
+		plancher: recurse(v.plancher)
+	}
+	return {
+		evaluate,
+		jsx: MecanismPlancher,
+		explanation,
+		category: 'mecanism',
+		name: 'plancher',
+		type: 'numeric',
+		unit: explanation.valeur.unit
+	}
+}
+
+Plancher.nom = 'plancher'