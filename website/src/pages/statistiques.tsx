46507d4844569f389fc8f3ea6308eec29c777c60
Maxime Quandalle <maxime@quandalle.com>
2021-11-30 09:12:03 +0100

    Ajout d'un suivi d'audience avec Plausible

---
+++ website/src/pages/statistiques.tsx
@@ -0,0 +1,65 @@
+import React from 'react'
+import Layout from '@theme/Layout'
+import useThemeContext from '@theme/hooks/useThemeContext'
+
+export default function Statistiques() {
+	return (
+		<Layout title="Statistiques">
+			<main style={{ marginTop: '40px' }}>
+				<h1>Statistiques de consultation</h1>
+				<IFrame />
+				<OptOutForm />
+			</main>
+		</Layout>
+	)
+}
+
+function IFrame() {
+	const { isDarkTheme } = useThemeContext()
+
+	return (
+		<>
+			<iframe
+				plausible-embed=""
+				src={`https://plausible.io/share/publi.codes?auth=EA2slHSJ5y9FMHp5RC9ip&embed=true&theme=${
+					isDarkTheme ? 'dark' : 'light'
+				}&background=transparent`}
+				scrolling="no"
+				loading="lazy"
+				frameBorder="0"
+				style={{ width: '1px', height: '1600px', minWidth: '100%' }}
+			></iframe>
+			<script async src="https://plausible.io/js/embed.host.js"></script>
+		</>
+	)
+}
+
+function OptOutForm() {
+	const optedOut =
+		typeof window !== 'undefined' && localStorage.plausible_ignore === 'true'
+	function optOutTracking(evt) {
+		if (evt.target.checked) {
+			localStorage.plausible_ignore = true
+		} else {
+			localStorage.clear()
+		}
+	}
+
+	return (
+		<div>
+			<p>
+				Nous suivons nos statistiques de consultation de manière anonyme sans
+				utiliser de cookies. Vous pouvez vous soustraire de ce suivi en cochant
+				la case ci-dessous :
+			</p>
+			<label>
+				<input
+					type="checkbox"
+					onChange={optOutTracking}
+					defaultChecked={optedOut}
+				/>{' '}
+				Je ne souhaite pas participer au suivi d’audience
+			</label>
+		</div>
+	)
+}
