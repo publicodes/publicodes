2492da9b89eb792326fbc795cca4bfdc00f7eeb7
Johan Girod <johangirod@gmail.com>
2022-05-03 18:29:01 +0200

    Remplace les mécanismes applicable et non applicable

--- packages/core/source/mecanisms/applicable.ts
+++ packages/core/source/mecanisms/applicable.ts
@@ -1,46 +1,21 @@
-import { EvaluationFunction } from '..'
-import { ASTNode, EvaluatedNode } from '../AST/types'
-import { registerEvaluationFunction } from '../evaluationFunctions'
-import parse from '../parse'
+import { createParseInlinedMecanism } from './utils'
 
-export type ApplicableSiNode = {
-	explanation: {
-		condition: ASTNode
-		valeur: ASTNode
+export default createParseInlinedMecanism(
+	'applicable si',
+	{
+		'applicable si': {},
+		valeur: {},
+	},
+	{
+		condition: {
+			si: 'applicable si != non',
+			alors: 'valeur',
+			sinon: {
+				constant: {
+					type: undefined,
+					nodeValue: null,
+				},
+			},
+		},
 	}
-	nodeKind: 'applicable si'
-}
-
-const evaluate: EvaluationFunction<'applicable si'> = function (node) {
-	const explanation = { ...node.explanation }
-	const condition = this.evaluate(explanation.condition)
-	let valeur = explanation.valeur
-	if (condition.nodeValue !== false) {
-		valeur = this.evaluate(valeur)
-	}
-	return {
-		...node,
-		nodeValue:
-			condition.nodeValue === undefined
-				? undefined
-				: condition.nodeValue === null || condition.nodeValue === false
-				? null
-				: (valeur as EvaluatedNode)?.nodeValue,
-		explanation: { valeur, condition },
-		...('unit' in valeur && { unit: valeur.unit }),
-	}
-}
-parseApplicable.nom = 'applicable si' as const
-
-export default function parseApplicable(v, context) {
-	const explanation = {
-		valeur: parse(v.valeur, context),
-		condition: parse(v[parseApplicable.nom], context),
-	}
-	return {
-		explanation,
-		nodeKind: parseApplicable.nom,
-	}
-}
-
-registerEvaluationFunction(parseApplicable.nom, evaluate)
+)
