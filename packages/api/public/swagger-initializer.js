33302b79bd6898108f671f67de5984bfca8893bc
Jérémy Rialland <wiinxt@gmail.com>
2022-05-10 13:09:17 +0200

    Configure swagger-ui

---
+++ packages/api/public/swagger-initializer.js
@@ -0,0 +1,15 @@
+window.onload = function() {
+  window.ui = SwaggerUIBundle({
+    url: "openapi.yaml",
+    dom_id: '#swagger-ui',
+    deepLinking: true,
+    presets: [
+      SwaggerUIBundle.presets.apis,
+      SwaggerUIStandalonePreset
+    ],
+    plugins: [
+      SwaggerUIBundle.plugins.DownloadUrl
+    ],
+    layout: "StandaloneLayout"
+  });
+};
