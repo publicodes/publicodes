5ed9c9934d90353d8f565105786a2a193d7f193f
Jérémy Rialland <wiinxt@gmail.com>
2022-05-16 14:58:03 +0200

    Convert api package to esm module Add route /openapi.json Add an option (`customOpenapi`) to customize /openapi.json Fixed a bug when adding a prefix to the middleware that caused the validator to not work

--- packages/api/source/middleware/koa.ts
+++ packages/api/source/middleware/koa.ts
@@ -2,11 +2,14 @@ import Router from '@koa/router'
 import { Context, Next } from 'koa'
 import koaBody from 'koa-body'
 import OAPIValidator from 'openapi-validator-middleware'
-import { openapiPath } from '../index'
-import * as routes from '../routes'
-import { Expressions, NewEngine, Situation } from '../types'
+import { openapi, openapiPath } from '../index.js'
+import * as routes from '../route/index.js'
+import { Expressions, NewEngine, Situation } from '../types.js'
+import { mergeDeep } from '../utils.js'
 
 const InputValidationError = OAPIValidator.InputValidationError
+
+type koaValidate = (ctx: Record<string, any>, next: Function) => Promise<void> // koa
 
 const applyKoaBodyIfNotPresent: Router.Middleware<{}, {}> = (
 	ctx: Context,
@@ -24,17 +27,52 @@ interface EvaluateBody {
 	situation?: Situation
 }
 
-export default function publicodesAPI(newEngine: NewEngine) {
+interface Options {
+	/**
+	 * Allows overriding the json returned by the /openapi.json endpoint.
+	 */
+	customOpenapi?: Record<string, unknown>
+}
+
+export default function publicodesAPI(
+	newEngine: NewEngine,
+	{ customOpenapi }: Options = {}
+) {
 	const router = new Router()
 
+	// The validator uses the original openapi.json, not the custom one
 	const OAPIValidatorMiddleware = OAPIValidator.getNewMiddleware(openapiPath, {
 		framework: 'koa',
 	})
 
+	/**
+	 * This middleware wrap OAPIValidatorMiddleware.validate method
+	 * to remove prefix in url if there is one in _matchedRoute.
+	 * @param route The OAPI route
+	 * @returns
+	 */
+	const validateInput =
+		(route: string | ((ctx: Context) => string)) =>
+		async (ctx: Context, next: Next) => {
+			const backupMatchedRoute = ctx._matchedRoute
+
+			ctx._matchedRoute = typeof route === 'string' ? route : route(ctx)
+			await (OAPIValidatorMiddleware.validate as koaValidate)(ctx, () => {})
+			ctx._matchedRoute = backupMatchedRoute
+
+			return await next()
+		}
+
+	const mergedOpenapi = customOpenapi
+		? mergeDeep(openapi, customOpenapi)
+		: openapi
+
 	router
 		.use(async (ctx: Context, next: Next) => {
 			try {
-				return await next()
+				const ret = await next()
+
+				return ret
 			} catch (err) {
 				if (!(err instanceof InputValidationError)) {
 					throw err
@@ -46,11 +84,9 @@ export default function publicodesAPI(newEngine: NewEngine) {
 		.post(
 			'/evaluate',
 			applyKoaBodyIfNotPresent,
-			OAPIValidatorMiddleware.validate,
+			validateInput('/evaluate'),
 			async (ctx: Context) => {
 				const { situation, expressions } = ctx.request.body as EvaluateBody
-
-				console.log(ctx.request.body, situation, expressions)
 
 				if (expressions) {
 					const evaluateResult = routes.evaluate(newEngine, {
@@ -63,16 +99,26 @@ export default function publicodesAPI(newEngine: NewEngine) {
 				}
 			}
 		)
-		.get('/rules', OAPIValidatorMiddleware.validate, (ctx: Context) => {
+		.get('/rules', validateInput('/rules'), (ctx: Context) => {
 			ctx.type = 'application/json'
 			ctx.body = routes.rules(newEngine)
 		})
-		.get('/rules/:rule', OAPIValidatorMiddleware.validate, (ctx: Context) => {
-			const { rule } = ctx.params
+		.get(
+			'/rules/:rule',
+			validateInput((ctx) => `/rules/${ctx.params.rule}`),
+			(ctx: Context) => {
+				const { rule } = ctx.params
 
+				ctx.type = 'application/json'
+				ctx.body = routes.rulesId(newEngine, rule)
+			}
+		)
+		.get('/openapi.json', (ctx: Context) => {
 			ctx.type = 'application/json'
-			ctx.body = routes.rulesId(newEngine, rule)
+			ctx.body = mergedOpenapi
 		})
 
-	return router.routes()
+	const routesMiddleware = router.routes()
+
+	return routesMiddleware
 }
