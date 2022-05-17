4bb2174f695a13fb869675c280bce98399ff09cd
Jérémy Rialland <wiinxt@gmail.com>
2022-05-17 18:59:38 +0200

    Use typescript 4.7 rc for api Remove iife package Fix prepack script

--- packages/api/tsup.config.ts
+++ packages/api/tsup.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'tsup'
 export default defineConfig([
 	{
 		entry: ['source/index.ts'],
-		format: ['cjs', 'esm', 'iife'],
+		format: ['cjs', 'esm'],
 		globalName: 'publicodes_api',
 		target: 'es2022',
 		clean: true,
