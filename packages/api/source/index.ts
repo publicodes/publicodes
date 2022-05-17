8f1a13c1b99ecf1fe2184f681764ece1b869dc6f
Jérémy Rialland <wiinxt@gmail.com>
2022-05-17 14:22:01 +0200

    Remove /openapi.json endpoint

--- packages/api/source/index.ts
+++ packages/api/source/index.ts
@@ -3,8 +3,11 @@ import { fileURLToPath } from 'url'
 export { koaMiddleware } from './middleware/index.js'
 
 // @ts-ignore
-export { default as openapi } from './openapi.json'
+import type { default as openapiType } from './openapi.json'
 
 const __dirname = dirname(fileURLToPath(import.meta.url))
 
 export const openapiPath = `${__dirname}/openapi.json`
+
+export const openapi = async () =>
+	(await import(openapiPath)).default as typeof openapiType
