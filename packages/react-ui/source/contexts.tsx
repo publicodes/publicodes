647211cba28b50e933a86f78a7779316bf38d6d4
Jérémy Rialland <wiinxt@gmail.com>
2022-09-05 19:01:21 +0200

    Refacto developer section of documentation

--- packages/react-ui/source/contexts.tsx
+++ packages/react-ui/source/contexts.tsx
@@ -1,35 +1,69 @@
 import Engine from 'publicodes'
-import React, { createContext } from 'react'
+import { ComponentProps, ComponentType, createContext, ReactNode } from 'react'
+import { Accordion, AccordionProps, Code, CodeProps } from './component'
 import References from './rule/References'
 
+type PartialRequired<T, R extends keyof T> = Omit<T, R> & Required<Pick<T, R>>
+
+export type AccordionItem = { title: string; id: string; children: ReactNode }
+
 export type SupportedRenderers = {
-	Link: React.ComponentType<{
-		children: React.ReactNode
+	Link?: ComponentType<{
+		children: ReactNode
 		to: string
 		title?: string
 	}>
-	Head?: React.ComponentType<{ children: React.ReactNode }>
+	Head?: ComponentType<{ children: ReactNode }>
 
 	/**
 	 * Used to render a rule description or title. Useful to parse markdown, links
 	 * or emojies.
 	 */
-	Text?: React.ComponentType<{ children: string }>
+	Text?: ComponentType<{ children: string }>
 	References?: typeof References
+	/**
+	 * Accordion used for developer documentation.
+	 */
+	Accordion?: ComponentType<AccordionProps>
+	/**
+	 * Block of code in pre
+	 */
+	Code?: ComponentType<CodeProps>
 }
 
-export const DefaultTextRenderer: React.ComponentType<{ children: string }> = ({
+const DefaultTextRenderer = ({
 	children,
-}) => <>{children}</>
+}: ComponentProps<RenderersCtx['Text']>) => <p>{children}</p>
+
+const DefaultLinkRenderer = (props: ComponentProps<RenderersCtx['Link']>) => (
+	<a {...props} />
+)
+
+export type RenderersCtx = PartialRequired<
+	SupportedRenderers,
+	'References' | 'Text' | 'Code' | 'Link' | 'Accordion'
+>
+
+export const defaultRenderers = (renderers: SupportedRenderers = {}) => {
+	const base: RenderersCtx = {
+		References,
+		Text: DefaultTextRenderer,
+		Code,
+		Accordion,
+		Link: DefaultLinkRenderer,
+	}
+
+	return Object.fromEntries(
+		[...Object.keys(base), ...Object.keys(renderers)]
+			.map((key) => [key, renderers[key] ?? base[key]])
+			.filter(([, val]) => val)
+	) as RenderersCtx
+}
+
+export const RenderersContext = createContext<RenderersCtx>(defaultRenderers())
 
 export const BasepathContext = createContext<string>('/documentation')
+
 export const EngineContext = createContext<Engine<string> | undefined>(
 	undefined
 )
-export const RenderersContext = createContext<
-	Partial<Omit<SupportedRenderers, 'Text'>> &
-		Required<Pick<SupportedRenderers, 'Text'>>
->({
-	References,
-	Text: DefaultTextRenderer,
-})
