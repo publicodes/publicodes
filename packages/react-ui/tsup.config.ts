5bb471a3f6428f3df32bf837747d76cc5d4d3507
Jérémy Rialland <wiinxt@gmail.com>
2022-11-29 12:26:07 +0100

    Remove unecessary config

--- packages/react-ui/tsup.config.ts
+++ packages/react-ui/tsup.config.ts
@@ -2,10 +2,9 @@ import { defineConfig } from 'tsup'
 
 export default defineConfig({
 	entry: ['source/index.tsx'],
-	format: ['cjs', 'esm', 'iife'],
+	format: ['cjs', 'esm'],
 	globalName: 'publicodesReact',
 	sourcemap: true,
 	clean: true,
 	dts: true,
-	inject: ['./react-shim.js'],
 })
