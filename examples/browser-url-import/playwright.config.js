7f9d6637a3e7327ffdbe9edd874541171a5f32ab
Maxime Quandalle <maxime@quandalle.com>
2022-12-03 15:38:44 +0100

    add integration test

---
+++ examples/browser-url-import/playwright.config.js
@@ -0,0 +1,38 @@
+// @ts-check
+import { devices } from '@playwright/test'
+
+/**
+ * @see https://playwright.dev/docs/test-configuration
+ * @type {import('@playwright/test').PlaywrightTestConfig}
+ */
+const config = {
+	testDir: './tests',
+	timeout: 30 * 1000,
+	expect: {
+		timeout: 5000,
+	},
+	fullyParallel: true,
+	forbidOnly: !!process.env.CI,
+	retries: process.env.CI ? 2 : 0,
+	workers: process.env.CI ? 1 : undefined,
+	reporter: 'html',
+	use: {
+		actionTimeout: 0,
+		baseURL: 'http://localhost:5173',
+		trace: 'on-first-retry',
+	},
+	webServer: {
+		command: 'yarn dev',
+		url: 'http://localhost:5173',
+	},
+	projects: [
+		{
+			name: 'chromium',
+			use: {
+				...devices['Desktop Chrome'],
+			},
+		},
+	],
+}
+
+export default config
