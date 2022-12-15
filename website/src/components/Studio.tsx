1ab3d48e21e083777e2feb696841f801fcb0eb2d
Mael <mael.thomas@data.gouv.fr>
2022-12-15 15:34:50 +0100

    Amélioration de l'avertissement de sécurité

--- website/src/components/Studio.tsx
+++ website/src/components/Studio.tsx
@@ -16,7 +16,9 @@ const { decodeRuleName } = utils
 
 const EXAMPLE_CODE = `
 # Bienvenue dans le bac à sable publicodes !
-# ⚠️ Le bac à sable est utile pour expérimenter, mais assurez-vous que votre code soit stocké de façon sécurisé ailleurs, par exemple sur un dépôt Github
+# ⚠️ Le bac à sable est utile pour expérimenter, mais : 
+# - fiabilité: assurez-vous rapidement que votre code soit stocké de façon sécurisé ailleurs, par exemple sur un dépôt Github
+# - sécurité: ne l'utilisez pas pour du code secret
 #
 # Pour en savoir plus sur le langage :
 # => https://publi.codes/docs/principes-de-base
