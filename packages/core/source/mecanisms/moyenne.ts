503b71e98ac8cf50d556d2c468cc172ea1f58fa4
Emile Rolley <emile.rolley@tuta.io>
2023-04-03 12:43:13 +0200

    refactor(core): simplify the `moyenne` mechansim

--- packages/core/source/mecanisms/moyenne.ts
+++ packages/core/source/mecanisms/moyenne.ts
@@ -1,8 +1,5 @@
-import { ASTNode, PublicodesExpression } from '..'
-import {
-	createParseInlinedMecanismWithArray,
-	notApplicableNode,
-} from './inlineMecanism'
+import { PublicodesExpression } from '..'
+import { createParseInlinedMecanismWithArray } from './inlineMecanism'
 import { reduceToSumNodes } from './somme'
 
 export default createParseInlinedMecanismWithArray(
@@ -16,52 +13,15 @@ export default createParseInlinedMecanismWithArray(
 		return {
 			'/': [
 				reduceToSumNodes(valeurs),
-				reduceToSumNodes(
-					// To only count the applicable nodes
-					valeurs.map((val) => changeTheNodeToOne(val as ASTNode))
-				),
+				reduceToSumNodes(valeurs.map(oneIfApplicable)),
 			],
 		}
 	}
 )
 
-function changeTheNodeToOne(value: ASTNode): ASTNode {
-	switch (value.nodeKind) {
-		case 'reference':
-		case 'constant':
-			return value.rawNode == null
-				? value
-				: {
-						nodeKind: 'constant',
-						nodeValue: 1,
-						type: 'number',
-				  }
-		case 'unité':
-			return {
-				...value,
-				explanation: changeTheNodeToOne(value.explanation),
-				unit: { numerators: [], denominators: [] },
-			}
-		case 'condition': {
-			return {
-				...value,
-				explanation: {
-					...value.explanation,
-					alors: changeTheNodeToOne(value.explanation.alors),
-					sinon: changeTheNodeToOne(value.explanation.sinon),
-				},
-			}
-		}
-		case 'variations': {
-			return {
-				...value,
-				explanation: value.explanation.map((v) => ({
-					...v,
-					consequence: changeTheNodeToOne(v.consequence),
-				})),
-			}
-		}
-		default:
-			return value
+function oneIfApplicable(exp: PublicodesExpression): PublicodesExpression {
+	return {
+		'applicable si': { 'est applicable': exp },
+		valeur: 1,
 	}
 }
