18ec66f0a69d3930164d04a07a0af7cf6e48a173
Johan Girod <dev@johangirod.com>
2023-04-04 17:46:41 +0200

    feat(react-ui) pass down prop 'dottedName' to 'Reference' component in rule page (#362)

--- packages/react-ui/source/rule/References.tsx
+++ packages/react-ui/source/rule/References.tsx
@@ -1,10 +1,14 @@
 import { capitalise0 } from 'publicodes'
 
 type ReferencesProps = {
-	references: Record<string, string>
+	references?: Record<string, string>
+	dottedName?: string // Useful for extending reference component in userdom
 }
 
 export default function References({ references }: ReferencesProps) {
+	if (!references) {
+		return null
+	}
 	return (
 		<ul>
 			{Object.entries(references).map(([name, link]) => (
