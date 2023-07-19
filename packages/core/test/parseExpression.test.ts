47ae1504a3ae1939a1bfc19bbe6c91fc4207a3e4
Emile Rolley <emile.rolley@tuta.io>
2023-07-19 21:27:02 +0200

    test(core): add minimal test to use parseExpression

---
+++ packages/core/test/parseExpression.test.ts
@@ -0,0 +1,20 @@
+import { expect } from 'chai'
+import { parseExpression } from '../source/index'
+
+describe("Enables external codebases to use publicodes's expression parser", () => {
+	it('should parse a basic expression', () => {
+		const parsed = parseExpression('équipement * facteur', 'foo . bar')
+		expect(JSON.stringify(parsed)).to.equal(
+			JSON.stringify({
+				'*': [
+					{
+						variable: 'équipement',
+					},
+					{
+						variable: 'facteur',
+					},
+				],
+			})
+		)
+	})
+})
