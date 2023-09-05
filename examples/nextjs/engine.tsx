8d4ebaa8bcdcb820fb65a578eabeab24f9cdc73f
Johan Girod <dev@johangirod.com>
2023-09-05 19:10:53 +0200

    Add an example for nextJS && update to beta 72

---
+++ examples/nextjs/engine.tsx
@@ -0,0 +1,19 @@
+import Engine from 'publicodes'
+import yaml from 'yaml'
+
+export default new Engine(
+	yaml.parse(`
+
+prix:
+  avec:
+    carottes: 2€/kg
+    champignons: 5€/kg
+    avocat: 2€/avocat
+
+dépenses primeur:
+  somme:
+    - prix . carottes * 1.5 kg
+    - prix . champignons * 500g
+    - prix . avocat * 3 avocat
+`)
+)
--- packages/react-ui/source/hooks.ts
+++ packages/react-ui/source/hooks.ts
@@ -2,6 +2,7 @@ import { useContext } from 'react'
 import { EngineContext } from './contexts'
 
 export const useEngine = () => {
+	console.log('tya', EngineContext)
 	const engine = useContext(EngineContext)
 	if (!engine) {
 		throw new Error('Engine expected')
