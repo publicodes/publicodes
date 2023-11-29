14681867d5550a6f1c2b33541194b45765b2be14
Johan Girod <dev@johangirod.com>
2023-11-29 20:17:33 +0100

    feat: ajoute un ordre de priorité dans remplace (#424)
    BREAKING CHANGE : si plusieurs remplacements sont applicables pour une
    même référence, l'ordre de priorité est défini par l'attribut `priorité`
    ou par l'ordre alphabétique inverse
    
    fix #313

--- packages/core/source/mecanisms/product.ts
+++ packages/core/source/mecanisms/product.ts
@@ -3,18 +3,18 @@ import { defaultNode } from '../evaluationUtils'
 import { createParseInlinedMecanismWithArray } from './inlineMecanism'
 
 export function reduceToProduitNodes(
-  valeurs: Array<PublicodesExpression>
+	valeurs: Array<PublicodesExpression>
 ): PublicodesExpression {
-  return valeurs.reduce((acc, value) => ({ '*': [value, acc] }), defaultNode(1))
+	return valeurs.reduce((acc, value) => ({ '*': [value, acc] }), defaultNode(1))
 }
 
 export default createParseInlinedMecanismWithArray(
-  'produit',
-  {
-    valeur: { type: 'liste' },
-  },
-  ({ valeur }) => ({
-    valeur: reduceToProduitNodes([...(valeur as Array<PublicodesExpression>)]),
-    "simplifier l'unité": 'oui',
-  })
+	'produit',
+	{
+		valeur: { type: 'liste' },
+	},
+	({ valeur }) => ({
+		valeur: reduceToProduitNodes([...(valeur as Array<PublicodesExpression>)]),
+		"simplifier l'unité": 'oui',
+	})
 )
