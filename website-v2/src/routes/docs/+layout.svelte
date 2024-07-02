cea79bb5be9c7d93115d79c800b853e02a38c4be
Johan Girod <dev@johangirod.com>
2024-07-02 21:48:14 +0200

    feat: ajoute un menu mobile pour la navigation dans les docs

--- website-v2/src/routes/docs/+layout.svelte
+++ website-v2/src/routes/docs/+layout.svelte
@@ -1,7 +1,9 @@
 <script lang="ts">
+    import { afterNavigate } from '$app/navigation';
     import { page } from '$app/stores';
     import MenuLink from '$lib/ui/menu-link.svelte';
     import type { Snippet } from 'svelte';
+    import { fly } from 'svelte/transition';
     import Page from '../+page.svelte';
     type Props = {
         children: Snippet;
@@ -30,36 +32,36 @@
     const currentPageMetadata = $derived(
         data.docPages.find((page) => `/docs${page.slug}` === $page.url.pathname)?.metadata
     );
-
+    let showMobileMenuLeft = $state(false);
+    let showMobileMenuRight = $state(false);
+    afterNavigate(() => {
+        showMobileMenuLeft = false;
+        showMobileMenuRight = false;
+    });
     // class="prose  mx-auto mt-8 max-w-none flex-1 flex-shrink p-8 xl:max-w-screen-md"
 </script>
 
-<div class="flex max-h-full items-start">
-    <nav class="sticky top-16 min-w-72 pr-4">
-        <ul>
-            {#each entryPages as { slug, metadata }}
-                <MenuLink href={`/docs${slug}`}>
-                    {metadata.menu_title || metadata.title}
-                    {#snippet submenu()}
-                        {#each getChildPage(slug) as { slug: childSlug, metadata }}
-                            <MenuLink href={`/docs${childSlug}`}>
-                                {metadata.menu_title || metadata.title}
-                            </MenuLink>
-                        {/each}
-                    {/snippet}
-                </MenuLink>
-            {/each}
-        </ul>
-    </nav>
-    <article
-        class="prose 2xl:prose-lg mx-auto max-w-screen-sm border-l px-8 py-16 xl:max-w-screen-md 2xl:max-w-4xl"
-    >
-        <h1>{currentPageMetadata.title}</h1>
-        {@render children()}
-    </article>
-    <nav class="sticky top-16 max-h-screen w-64 overflow-auto pt-16 text-sm">
-        {#if currentPageMetadata.headings.length}
-            <h2 class="not-prose my-2 p-2 uppercase text-slate-500">Sur cette page :</h2>
+{#snippet MenuLeft()}
+    <ul>
+        {#each entryPages as { slug, metadata }}
+            <MenuLink href={`/docs${slug}`}>
+                {metadata.menu_title || metadata.title}
+                {#snippet submenu()}
+                    {#each getChildPage(slug) as { slug: childSlug, metadata }}
+                        <MenuLink href={`/docs${childSlug}`}>
+                            {metadata.menu_title || metadata.title}
+                        </MenuLink>
+                    {/each}
+                {/snippet}
+            </MenuLink>
+        {/each}
+    </ul>
+{/snippet}
+
+{#snippet MenuRight()}
+    {#if currentPageMetadata.headings.length}
+        <div class="mx-2">
+            <h2 class="not-prose my-2 p-2 uppercase text-slate-500">Sur cette page</h2>
             <ul>
                 {#each currentPageMetadata.headings as { title, slug }}
                     <li class="p-2">
@@ -67,7 +69,78 @@
                     </li>
                 {/each}
             </ul>
-        {/if}
+        </div>
+    {/if}
+{/snippet}
+
+<div class="flex max-h-full items-start">
+    <!-- MOBILE NAV -->
+    {#if showMobileMenuLeft}
+        <nav
+            class="fixed z-20 h-full overflow-auto border-r bg-white will-change-transform"
+            transition:fly={{ x: -100 }}
+        >
+            {@render MenuLeft()}
+        </nav>
+        <div class="fixed inset-0 z-10" onclick={() => (showMobileMenuLeft = false)}></div>
+    {/if}
+
+    <nav class="sticky top-16 min-w-72 pr-4 max-md:hidden">{@render MenuLeft()}</nav>
+    <div class="self-stretch border-r"></div>
+    <div
+        class="mx-auto flex flex-1 flex-col px-4 pt-8 transition-all 2xl:prose-lg max-md:max-w-full md:px-8 xl:max-w-screen-md 2xl:max-w-4xl"
+    >
+        <div class="mb-4 flex justify-between gap-2">
+            <button
+                class="self flex gap-2 rounded-md border px-4 py-2 text-primary-700 hover:bg-slate-100 md:hidden"
+                onclick={() => (showMobileMenuLeft = true)}
+            >
+                <svg
+                    class="h-6 w-6"
+                    fill="none"
+                    stroke="currentColor"
+                    viewBox="0 0 24 24"
+                    xmlns="http://www.w3.org/2000/svg"
+                >
+                    <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        stroke-width="2"
+                        d="M4 6h16M4 12h16m-7 6h7"
+                    ></path>
+                </svg>Menu
+            </button>
+
+            <button
+                class="flex rounded-md border px-4 py-2 text-primary-700 hover:bg-slate-100 lg:hidden"
+                onclick={() => (showMobileMenuRight = true)}
+            >
+                <span>Sur cette page</span>
+            </button>
+        </div>
+        <article
+            class:blur-sm={showMobileMenuLeft || showMobileMenuRight}
+            class:opacity-50={showMobileMenuLeft || showMobileMenuRight}
+            class="prose py-8"
+        >
+            <h1>{currentPageMetadata.title}</h1>
+            {@render children()}
+        </article>
+    </div>
+
+    {#if showMobileMenuRight}
+        <nav
+            onclick={() => (showMobileMenuRight = false)}
+            class="fixed right-0 z-20 h-full overflow-auto border-l bg-white will-change-transform"
+            transition:fly={{ x: 100 }}
+        >
+            {@render MenuRight()}
+        </nav>
+        <div class="fixed inset-0 z-10" onclick={() => (showMobileMenuRight = false)}></div>
+    {/if}
+
+    <nav class="sticky top-16 max-h-screen w-64 overflow-auto pt-16 text-sm max-lg:hidden">
+        {@render MenuRight()}
     </nav>
 </div>
 
