87bf4113e6d2e30818ffd117d454d7abf8201548
Emile Rolley <emile.rolley@tuta.io>
2024-07-19 14:08:47 +0200

    fix(website): better go back link in the article layout

--- website-v2/src/routes/blog/(article)/+layout.svelte
+++ website-v2/src/routes/blog/(article)/+layout.svelte
@@ -1,16 +1,21 @@
 <script>
-    import Button from '$lib/ui/button.svelte';
     import { ArrowLeft } from 'lucide-svelte';
 </script>
 
 <article class="lg:py-18 flex justify-center px-6 py-14">
-    <div class="flex flex-col gap-16">
-        <div class="flex items-start">
-            <a href="/blog">
-                <Button type="secondary" icon={ArrowLeft} light>Retourner à la liste</Button>
+    <div class="flex flex-col lg:grid lg:grid-cols-5">
+        <div
+            class="col-span-1 mb-12 inline-flex max-h-4 justify-start lg:sticky lg:top-32 lg:justify-start"
+        >
+            <a
+                href="/blog"
+                class="flex items-center gap-2 text-lg text-primary-400 hover:text-primary-600"
+            >
+                <ArrowLeft size={24} strokeWidth={1.5} />
+                Retourner à la liste
             </a>
         </div>
-        <div class="prose lg:prose-lg xl:max-w-4xl">
+        <div class="prose col-span-3 lg:prose-lg xl:max-w-4xl">
             <slot></slot>
         </div>
     </div>
