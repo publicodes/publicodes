8462d7cc1a0badc461528003d29642f082fd5607
Emile Rolley <emile.rolley@tuta.io>
2025-01-28 19:23:27 +0100

    fix(tools): update getTypes to match the new "une possiblité" mechanism

--- packages/tools/src/compilation/ruleTypes.ts
+++ packages/tools/src/compilation/ruleTypes.ts
@@ -1,15 +1,15 @@
-import Engine, { ASTNode, reduceAST, RuleNode } from 'publicodes'
+import Engine from 'publicodes'
 
 export type RuleType =
-  | {
-      type: 'number' | 'date' | 'boolean' | 'string'
-      isNullable: boolean
-    }
-  | {
-      type: 'enum'
-      options: string[]
-      isNullable: boolean
-    }
+	| {
+			type: 'number' | 'date' | 'boolean' | 'string'
+			isNullable?: boolean
+	  }
+	| {
+			type: 'enum'
+			options: string[]
+			isNullable?: boolean
+	  }
 
 /**
  * Resolve the type for each rule in the parsed rules.
@@ -18,61 +18,36 @@ export type RuleType =
  * @returns A record with the rule name as key and the type as value.
  */
 export function resolveRuleTypes<RuleName extends string = string>(
-  engine: Engine<RuleName>,
+	engine: Engine<RuleName>,
 ): Record<string, RuleType> {
-  const parsedRules = engine.getParsedRules()
-  const ruleTypes: Record<RuleName, RuleType> = {} as any
+	const parsedRules = engine.getParsedRules()
+	const ruleTypes: Record<RuleName, RuleType> = {} as any
 
-  for (const ruleName in parsedRules) {
-    const rule = parsedRules[ruleName]
-    const ruleType = engine.context.nodesTypes.get(rule)
+	for (const ruleName in parsedRules) {
+		const rule = parsedRules[ruleName]
+		const ruleType = engine.context.nodesTypes.get(rule)
+		const possibilities = engine.getPossibilitiesFor(ruleName)
 
-    if (ruleType?.type === undefined) {
-      // Empty rule are considered as boolean
-      // NOTE: Should we consider a different type for empty rules?
-      ruleTypes[ruleName] = {
-        type: 'boolean',
-        isNullable: ruleType.isNullable,
-      }
-    } else if (ruleType.type === 'string') {
-      // Most of the time, a string rule is an enumeration
-      const options = collectOptions(rule)
+		if (possibilities) {
+			ruleTypes[ruleName] = {
+				type: 'enum',
+				options: possibilities.map(({ nodeValue }) => nodeValue.toString()),
+				isNullable: ruleType?.isNullable,
+			}
+		} else if (ruleType?.type === undefined) {
+			// Empty rule are considered as boolea
+			// NOTE: Should we consider a different type for empty rules?
+			ruleTypes[ruleName] = {
+				type: 'boolean',
+				isNullable: ruleType?.isNullable,
+			}
+		} else {
+			ruleTypes[ruleName] = {
+				type: ruleType.type,
+				isNullable: ruleType.isNullable,
+			}
+		}
+	}
 
-      if (options !== undefined) {
-        ruleTypes[ruleName] = {
-          type: 'enum',
-          options,
-          isNullable: ruleType.isNullable,
-        }
-      } else {
-        ruleTypes[ruleName] = {
-          type: 'string',
-          isNullable: ruleType.isNullable,
-        }
-      }
-    } else {
-      ruleTypes[ruleName] = {
-        type: ruleType.type,
-        isNullable: ruleType.isNullable,
-      }
-    }
-  }
-
-  return ruleTypes
-}
-
-/**
- * Returns the list of all options of a rule if it contains a `une possibilité`
- * mechanism.
- */
-export function collectOptions(rule: RuleNode): string[] | undefined {
-  return reduceAST(
-    (_, node: ASTNode) => {
-      if (node.nodeKind === 'une possibilité') {
-        return node['possibilités']
-      }
-    },
-    undefined,
-    rule,
-  )
+	return ruleTypes
 }
--- packages/tools/test/compilation/ruleTypes.test.ts
+++ packages/tools/test/compilation/ruleTypes.test.ts
@@ -1,69 +1,100 @@
-import Engine from 'publicodes'
+import Engine, { Rule } from 'publicodes'
+import { describe, expect, it } from 'vitest'
 import { RawRules } from '../../src'
 import { resolveRuleTypes } from '../../src/compilation/ruleTypes'
 
 const getTypes = (rawRules: RawRules) => {
-  const engine = new Engine(rawRules)
-  return resolveRuleTypes(engine)
+	const engine = new Engine(rawRules as Record<string, Rule>)
+	return resolveRuleTypes(engine)
 }
 
 describe('resolveRuleType > constant rules', () => {
-  it('empty', () => {
-    const rawRules = { rule: null }
-    expect(getTypes(rawRules)).toEqual({
-      rule: { type: 'boolean', isNullable: false },
-    })
-  })
+	it('empty', () => {
+		const rawRules = { rule: null }
+		expect(getTypes(rawRules)).toEqual({
+			rule: { type: 'boolean', isNullable: false },
+		})
+	})
 
-  it('number', () => {
-    const rawRules = { rule: 42 }
-    expect(getTypes(rawRules)).toEqual({
-      rule: { type: 'number', isNullable: false },
-    })
-  })
+	it('number', () => {
+		const rawRules = { rule: 42 }
+		expect(getTypes(rawRules)).toEqual({
+			rule: { type: 'number', isNullable: false },
+		})
+	})
 
-  it('date', () => {
-    const rawRules = { rule: '01/03/2022' }
-    expect(getTypes(rawRules)).toEqual({
-      rule: { type: 'date', isNullable: false },
-    })
-  })
+	it('date', () => {
+		const rawRules = { rule: '01/03/2022' }
+		expect(getTypes(rawRules)).toEqual({
+			rule: { type: 'date', isNullable: false },
+		})
+	})
 
-  it('boolean', () => {
-    const rawRules = { vrai: 'oui', faux: 'non' }
-    expect(getTypes(rawRules)).toEqual({
-      vrai: { type: 'boolean', isNullable: false },
-      faux: { type: 'boolean', isNullable: false },
-    })
-  })
+	it('boolean', () => {
+		const rawRules = { vrai: 'oui', faux: 'non' }
+		expect(getTypes(rawRules)).toEqual({
+			vrai: { type: 'boolean', isNullable: false },
+			faux: { type: 'boolean', isNullable: false },
+		})
+	})
 
-  it('string', () => {
-    const rawRules = { rule: "'value 1'" }
-    expect(getTypes(rawRules)).toEqual({
-      rule: { type: 'string', isNullable: false },
-    })
-  })
+	it('string', () => {
+		const rawRules = { rule: "'value 1'" }
+		expect(getTypes(rawRules)).toEqual({
+			rule: { type: 'string', isNullable: false },
+		})
+	})
 
-  it('enum (une possibilité)', () => {
-    const rawRules = {
-      rule: {
-        'une possibilité': ['valeur 1', 'valeur 2'],
-        avec: {
-          'valeur 1': null,
-          'valeur 2': null,
-        },
-      },
-    }
-    expect(getTypes(rawRules)).toEqual({
-      rule: {
-        type: 'enum',
-        options: ['valeur 1', 'valeur 2'],
-        isNullable: false,
-      },
-      'rule . valeur 1': { type: 'boolean', isNullable: false },
-      'rule . valeur 2': { type: 'boolean', isNullable: false },
-    })
-  })
+	it('enum (une possibilité)', () => {
+		const rawRules = {
+			rule: {
+				'une possibilité': ['valeur 1', 'valeur 2'],
+				avec: {
+					'valeur 1': null,
+					'valeur 2': null,
+				},
+			},
+		}
+		expect(getTypes(rawRules)).toEqual({
+			rule: {
+				type: 'enum',
+				options: ['valeur 1', 'valeur 2'],
+				isNullable: false,
+			},
+			'rule . valeur 1': { type: 'boolean', isNullable: false },
+			'rule . valeur 2': { type: 'boolean', isNullable: false },
+		})
+	})
+
+	it('enum (une possibilité de nombre)', () => {
+		const rawRules = {
+			rule: {
+				'une possibilité': ['10 km', '20'],
+			},
+		}
+		expect(getTypes(rawRules)).toEqual({
+			rule: {
+				type: 'enum',
+				options: ['10', '20'],
+				isNullable: false,
+			},
+		})
+	})
+
+	it('enum (une possibilité de string)', () => {
+		const rawRules = {
+			rule: {
+				'une possibilité': ["'valeur 1'", "'valeur 2'"],
+			},
+		}
+		expect(getTypes(rawRules)).toEqual({
+			rule: {
+				type: 'enum',
+				options: ['valeur 1', 'valeur 2'],
+				isNullable: false,
+			},
+		})
+	})
 })
 
 // TODO: Add tests for the following cases:
