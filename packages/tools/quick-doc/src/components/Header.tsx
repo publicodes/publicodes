4c1bbfaa12f5fe0414678042b21666a9e1d5547a
Johan Girod <dev@johangirod.com>
2025-02-06 10:01:42 +0100

    fix: lint

--- packages/tools/quick-doc/src/components/Header.tsx
+++ packages/tools/quick-doc/src/components/Header.tsx
@@ -1,6 +1,6 @@
 import { Link } from 'react-router-dom'
 import { State } from '../app-state'
-import React from 'react';
+import React from 'react'
 export default function Header({
 	setSituation,
 	state,
