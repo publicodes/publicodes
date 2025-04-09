8671c7a314017afc5419a2afebc18a6deebca51a
Emile Rolley <emile.rolley@tuta.io>
2025-04-09 17:33:06 +0200

    feat(compiler): start to setup the compiler workflow

--- packages/compiler/bin/main.ml
+++ packages/compiler/bin/main.ml
@@ -1,1 +1,30 @@
-let () = print_endline "Hello, World!"
+let () =
+  match Sys.argv with
+  | [| _; file_path |] -> (
+      let open Compiler in
+      let open Core in
+      print_endline ("Reading file: " ^ file_path);
+      match Yaml_parser.parse_yaml file_path with
+      | Ok yaml ->
+          print_endline "Successfully parsed YAML file";
+          Yaml_parser.print_yaml yaml;
+          Yaml.to_json yaml
+          |> Result.map ~f:(fun json ->
+                 Yaml.to_string_exn json
+                 |> Utils.File.write_file
+                      ( Filename.basename file_path |> Filename.chop_extension
+                      |> fun base_name -> base_name ^ "-output.json" ))
+          |> ignore
+          (* let keys = *)
+          (*   Yaml.to_json yaml |> Result.map ~f:(fun json -> Yaml.Util.keys json) *)
+          (* in *)
+          (* Result.map keys ~f:(fun keys -> *)
+          (*     Result.map keys ~f:(fun keys -> *)
+          (*         List.iter keys ~f:(fun key -> print_endline ("Key: " ^ key)))) *)
+          (* |> ignore *)
+      | Error _ ->
+          print_endline "Failed to parse YAML file";
+          exit 1)
+  | _ ->
+      print_endline "Usage: ./main <file_path>";
+      exit 1
