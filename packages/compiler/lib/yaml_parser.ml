a9bec9fb7601b1d086d53276692d58b37dd5edfd
Emile Rolley <emile.rolley@tuta.io>
2025-04-09 17:33:06 +0200

    feat(compiler): start to setup the compiler workflow

---
+++ packages/compiler/lib/yaml_parser.ml
@@ -0,0 +1,8 @@
+let parse_yaml file_path =
+  let raw_content = Utils.File.read_file file_path in
+  Yaml.yaml_of_string raw_content
+
+let print_yaml yaml =
+  match Yaml.yaml_to_string ~encoding:`Utf8 yaml with
+  | Ok yaml_string -> Printf.printf "%s\n" yaml_string
+  | Error _ -> print_endline "Failed to convert YAML to string"
