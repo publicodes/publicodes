c6b12e81f604e7171367904f12d55855b670efe3
Johan Girod <johan@girod.email>
2025-04-09 19:35:03 +0200

    feat(compiler): first version of compiler

--- packages/compiler/bin/main.ml
+++ packages/compiler/bin/main.ml
@@ -5,26 +5,32 @@ let () =
       let open Core in
       print_endline ("Reading file: " ^ file_path);
       match Yaml_parser.parse_yaml file_path with
-      | Ok yaml ->
+      | Ok yaml -> (
           print_endline "Successfully parsed YAML file";
-          Yaml_parser.print_yaml yaml;
-          Yaml.to_json yaml
-          |> Result.map ~f:(fun json ->
-                 Yaml.to_string_exn json
-                 |> Utils.File.write_file
-                      ( Filename.basename file_path |> Filename.chop_extension
-                      |> fun base_name -> base_name ^ "-output.json" ))
-          |> ignore
-          (* let keys = *)
-          (*   Yaml.to_json yaml |> Result.map ~f:(fun json -> Yaml.Util.keys json) *)
-          (* in *)
-          (* Result.map keys ~f:(fun keys -> *)
-          (*     Result.map keys ~f:(fun keys -> *)
-          (*         List.iter keys ~f:(fun key -> print_endline ("Key: " ^ key)))) *)
-          (* |> ignore *)
+          match Parser.parse yaml with
+          | Ok program -> Format.printf "program: %a" Ast.pp_program program
+          | Error _err ->
+              print_endline "Error printing";
+              exit 1)
       | Error _ ->
           print_endline "Failed to parse YAML file";
           exit 1)
   | _ ->
       print_endline "Usage: ./main <file_path>";
       exit 1
+
+(* Yaml_parser.print_yaml yaml;
+Yaml.to_json yaml
+|> Result.map ~f:(fun json ->
+       Yaml.to_string_exn json
+       |> Utils.File.write_file
+            ( Filename.basename file_path |> Filename.chop_extension
+            |> fun base_name -> base_name ^ "-output.json" ))
+|> ignore *)
+(* let keys = *)
+(*   Yaml.to_json yaml |> Result.map ~f:(fun json -> Yaml.Util.keys json) *)
+(* in *)
+(* Result.map keys ~f:(fun keys -> *)
+(*     Result.map keys ~f:(fun keys -> *)
+(*         List.iter keys ~f:(fun key -> print_endline ("Key: " ^ key)))) *)
+(* |> ignore *)
---
+++ packages/compiler/lib/parser.ml
@@ -0,0 +1,50 @@
+open Core
+open Ast
+
+let get_key_exn = function
+  | `Alias str -> str
+  | `Scalar Yaml.{ value; _ } -> value
+  | _ -> raise (Failure "Internat error : Expected an alias or scalrar ")
+
+let parse_mechanism _value = Undefined
+
+let parse : Yaml.yaml -> (program, string) result = function
+  | `O { m_members = []; _ } -> Error "Empty file"
+  | `O { m_members; _ } ->
+      Ok
+        (List.map m_members ~f:(fun (key, value) ->
+             { name = get_key_exn key; value = parse_mechanism value }))
+  | _ -> failwith "todo"
+
+(*
+
+(yaml * yaml) list
+
+type scalar = {
+  anchor : string option;
+  tag : string option;
+  value : string;
+  plain_implicit : bool;
+  quoted_implicit : bool;
+  style : scalar_style;
+}
+
+type yaml =
+  [ `Scalar of scalar | `Alias of string | `A of sequence | `O of mapping ]
+
+and sequence = {
+  s_anchor : string option;
+  s_tag : string option;
+  s_implicit : bool;
+  s_members : yaml list;
+}
+
+and mapping = {
+  m_anchor : string option;
+  m_tag : string option;
+  m_implicit : bool;
+  m_members : (yaml * yaml) list;
+}
+
+
+*)
