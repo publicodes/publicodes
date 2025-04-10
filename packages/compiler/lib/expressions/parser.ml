5a8d0ae54bae0952c6223e33c06d7e3a7dcb4b17
Johan Girod <johan@girod.email>
2025-04-10 21:36:20 +0200

    feat: setup lexer and unit test

--- packages/compiler/lib/expressions/parser.ml
+++ packages/compiler/lib/expressions/parser.ml
@@ -2,4 +2,10 @@ open Utils
 
 (* TODO: reimplement the expression parser and handle localisation *)
 let parse str =
-  Loc.make_with ~filename:"test" ~col:0 ~row:0 (Ast.Const (String str))
+  Loc.make_with ~filename:"test" ~starts:(0, 0) (Ast.Const (String str))
+
+(* let lex str =
+  let tokens = Str.split (Str.regexp "[ \t\n]+") str in
+  List.map
+    (fun token -> (token, Loc.make_with ~filename:"test" ~col:0 ~row:0 ()))
+    tokens *)
