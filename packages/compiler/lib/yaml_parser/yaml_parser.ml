a57145c00d3d932f0c1444776d0a5f98de229105
Johan Girod <dev@johangirod.com>
2025-05-02 17:31:18 +0200

    V2/error reporting (#676)
    * Output logs without interupting compilation
    
    * Add warning for orphan rules

--- packages/compiler/lib/yaml_parser/yaml_parser.ml
+++ packages/compiler/lib/yaml_parser/yaml_parser.ml
@@ -1,9 +1,5 @@
 include Ast
 include Utils
 
-let parse (filename : string) (content : string) : (yaml, string) result =
-  (* Utility function to convert mark to position *)
-  (* Allow to use let%bind *)
-  match Parse.parse filename content with
-  | Ok result -> Ok result
-  | Error err -> Error (Format.asprintf "%a" Log.pp err)
+let parse ~(filename : string) (content : string) : yaml Output.t =
+  Parse.parse filename content
