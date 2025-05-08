5fdf2411725459808ecb2c821f408aa601b0fb90
Johan Girod <dev@johangirod.com>
2025-05-08 12:25:18 +0200

    V2/type check (#677)
    * feat: prepare adding type info to eval ast
    
    * feat: add type_database
    
    * feat: first version of type inference
    
    * wip
    
    * feat: type check return error with position

---
+++ packages/compiler/lib/eval/type_database.ml
@@ -0,0 +1,50 @@
+open Core
+
+type id = int [@@deriving sexp, show]
+
+let current_id = ref 0
+
+let next_id () =
+  let id = !current_id in
+  current_id := id + 1 ;
+  id
+
+type concrete_type = Number | String | Bool | Date
+[@@deriving sexp, compare, show]
+
+type value = Concrete of concrete_type | Link of id | Null
+[@@deriving sexp, show]
+
+type t = value array
+
+let mk () = Array.create ~len:!current_id Null
+
+let type_to_string = function
+  | Concrete Number ->
+      "Number"
+  | Concrete String ->
+      "String"
+  | Concrete Bool ->
+      "Bool"
+  | Concrete Date ->
+      "Date"
+  | Link id ->
+      Printf.sprintf "db[%s]" (string_of_int id)
+  | Null ->
+      "null"
+
+let pp fmt db =
+  let open Format in
+  (* Print header *)
+  fprintf fmt "@[<v 0>" ;
+  fprintf fmt "@\n" ;
+  fprintf fmt "┌───────────┬─────────────┐@\n" ;
+  fprintf fmt "│ id        │ type        │@\n" ;
+  fprintf fmt "├───────────┼─────────────┤@\n" ;
+  (* Print each entry *)
+  Array.iteri db ~f:(fun i v ->
+      let id_display = Printf.sprintf " %d " i in
+      fprintf fmt "│ %-9s │ %-11s │@\n" id_display (type_to_string v) ) ;
+  (* Print footer *)
+  fprintf fmt "└───────────┴─────────────┘@\n" ;
+  fprintf fmt "@]"
--- packages/compiler/lib/resolver/resolver.ml
+++ packages/compiler/lib/resolver/resolver.ml
@@ -4,40 +4,40 @@ open Common
 open Common.Shared_ast
 open Utils.Output
 
-let disambiguate_expr ~rule_names ~context_rule expr =
-  let rec resolve expr =
+let rec resolve_expr ~rule_names ~context_rule ((expr, pos) : 'a Shared_ast.expr)
+    =
+  let resolve_expr = resolve_expr ~rule_names ~context_rule in
+  let* expr =
     match expr with
     | Ref ref ->
-        let ref =
-          Pos.map ~f:(Rule_name.resolve ~rule_names ~current:context_rule) ref
-        in
+        let ref = Rule_name.resolve ~rule_names ~current:context_rule ref in
         let logs =
           match ref with
-          | None, pos ->
+          | None ->
               [Log.error ~pos ~kind:`Syntax "La référence n'existe pas"]
           | _ ->
               []
         in
         return ~logs (Ref ref)
     | BinaryOp (operator, left, right) ->
-        let* left = resolve left in
-        let* right = resolve right in
+        let* left = resolve_expr left in
+        let* right = resolve_expr right in
         return (BinaryOp (operator, left, right))
     | UnaryOp (operator, operand) ->
-        let* operand = resolve operand in
+        let* operand = resolve_expr operand in
         return (UnaryOp (operator, operand))
     | Const expr ->
         return (Const expr)
   in
-  resolve expr
+  return (expr, pos)
 
 let resolve_value ~rule_names ~context_rule expr =
   match expr with
-  | Undefined ->
-      return Undefined
   | Expr expr ->
-      let+ expr = disambiguate_expr ~rule_names ~context_rule expr in
+      let+ expr = resolve_expr ~rule_names ~context_rule expr in
       Expr expr
+  | Undefined pos ->
+      return (Undefined pos)
 
 let check_orphan_rules ~rule_names ast =
   let warn_if_orphan {name= name, pos; _} =
@@ -76,4 +76,4 @@ let to_resolved_ast ast =
     |> List.map ~f:(resolve_rule ~rule_names)
     |> from_list |> add_logs ~logs:orphan_logs
   in
-  (ast, rule_names)
+  ast
