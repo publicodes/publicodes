08b6e5f392702f544e64998033ceefeba6d658dc
Johan Girod <dev@johangirod.com>
2025-05-16 13:26:31 +0200

    Remove parser utility functions

--- packages/compiler/lib/parser/parser_utils.ml
+++ packages/compiler/lib/parser/parser_utils.ml
@@ -19,28 +19,6 @@ let parse_array ~pos
       fatal_error ~pos ~kind:`Syntax
         "Ce mécanisme doit contenir un tableau de valeurs"
 
-let only_one_of ~keys ~error_msg mapping : unit Output.t =
-  let keys =
-    List.filter_map mapping ~f:(fun (key, _) ->
-        let key_value = get_value key in
-        if List.mem ~equal:String.equal keys key_value then
-          Some (key_value, Pos.pos key)
-        else None )
-  in
-  match keys with
-  | [] | [_] ->
-      return ()
-  | (key, _) :: keys ->
-      let logs =
-        List.map
-          ~f:(fun (_, pos) ->
-            Log.error ~kind:`Syntax ~pos
-              ~hint:"Il y a peut-être une erreur d'indentation"
-              (Format.sprintf error_msg key) )
-          keys
-      in
-      (None, logs)
-
 let remove_double (mapping : mapping) : mapping Output.t =
   let seen_keys = ref (Set.empty (module String)) in
   let result_mapping = ref [] in
@@ -61,27 +39,3 @@ let remove_double (mapping : mapping) : mapping Output.t =
         seen_keys := Set.add !seen_keys key_value ;
         result_mapping := (key, value) :: !result_mapping ) ) ;
   return ~logs:!logs (List.rev !result_mapping)
-
-let order_keys ~(first_keys : string list) (mapping : mapping) =
-  let compare a b =
-    let a_idx =
-      List.findi
-        ~f:(fun _ key -> String.equal (get_value (fst a)) key)
-        first_keys
-    in
-    let b_idx =
-      List.findi
-        ~f:(fun _ key -> String.equal (get_value (fst b)) key)
-        first_keys
-    in
-    match (a_idx, b_idx) with
-    | Some a_idx, Some b_idx ->
-        Int.compare (fst a_idx) (fst b_idx)
-    | Some _, None ->
-        -1
-    | None, Some _ ->
-        1
-    | None, None ->
-        0
-  in
-  List.sort ~compare mapping
