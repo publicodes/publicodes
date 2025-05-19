08149037a1f6a0b1f6e8b370bdf09bf166be21d3
Johan Girod <dev@johangirod.com>
2025-05-19 10:48:41 +0200

    V2/contexte (#694)
    Add contexte mechanism for local variable overrides
    
    This adds a new mechanism that allows defining context variables within a
    rule, which can override or set new variables that are only used during
    the evaluation of that specific rule.
    
    Co-authored-by: Emile Rolley <emile.rolley@tuta.io>

--- packages/compiler/lib/compiler.mli
+++
@@ -1,7 +0,0 @@
-open Utils
-
-val compile_file : string -> filename:string -> string option * Log.t list
-(** [compile_file content ~filename] compiles the given [content] string
-		and returns the result of the compilation. The [filename] is used for
-		error reporting. The function returns a tuple of an option containing the
-		compiled content and a list of logs. *)
