9f05d84a0c07a2f349b609d2aaa3ec2b94a0183d
Willow Barraco <contact@willowbarraco.fr>
2026-03-03 11:59:08 +0100

    Support for yaml config file
    This add a -c command argument to pass a config file path. This allow
    the user to specify multiple compilation targets. A default value
    "publicodes.yaml" is used when absent. If both inputs files, and
    config file are given by the user, the config file is ignored.
    
    Currently the watch mode is not supported when building multiple
    targets. An error message is displayed to the user for now.
    
    The yaml format is simple, and specify the build targets:
    
    	targets:
    	  - output: test.js.local
    	    inputs:
    	      - test.publicodes.local
    	    type: js
    	    default_to_public: false
    
    All fields are checked, and a corresponding error message is build to
    the notice the user on invalid format. Ex:
    
    Error: Parsing config: "targets" field error: "inputs" field error: Expected a string value

--- packages/compiler/bin/compile.ml
+++ packages/compiler/bin/compile.ml
@@ -3,6 +3,14 @@ open Cmdliner
 open Utils
 open Utils.Output
 
+type target_type = [`Js | `Debug_eval_tree]
+
+type target =
+  { output_file: string
+  ; input_files: string list
+  ; output_type: target_type
+  ; default_to_public: bool }
+
 let cmd_exit (logs : Log.t list) : Cmd.Exit.code =
   let contains_error logs =
     List.exists logs ~f:(fun log ->
@@ -20,3 +28,14 @@ let compile ~input_files ~output_type ~output_file ~default_to_public =
       exit_code
   | None ->
       Cmd.Exit.some_error
+
+let compile_target target =
+  let {output_file; input_files; output_type; default_to_public} = target in
+  compile ~input_files ~output_file ~output_type ~default_to_public
+
+let rec compile_targets targets =
+  match targets with
+  | [] ->
+      Cmd.Exit.ok
+  | head :: tail -> (
+    match compile_target head with 0 -> compile_targets tail | exit -> exit )
