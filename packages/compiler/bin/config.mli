dd7c3d34db96e4f04ddba4899aff19c48218b0f4
Willow Barraco <contact@willowbarraco.fr>
2026-03-09 09:41:22 +0100

    compiler: support for "init" subcommand
    The init subcommand generate and write a default configuration file.
    
    Co-authored-by: Emile Rolley <emile.rolley@tuta.io>

--- packages/compiler/bin/config.mli
+++ packages/compiler/bin/config.mli
@@ -15,9 +15,17 @@
 (** Represent a build target *)
 type t = {targets: Compile.t list}
 
+(** {2 Parsing} *)
+
 val parse_yaml : in_channel -> (t, [`Msg of string]) result
-(** [pare_yaml in_channel] parse a config from a yaml channel *)
+(** [parse_yaml ic] parse a config from a yaml channel *)
 
 val parse : string -> (t, [`Msg of string]) result
-(** [pare config] parse a config from a file path. The parser is guessed
+(** [parse config_path] parse a config from a file path. The parser is guessed
   from the file extension. *)
+
+(** {2 Defaults} *)
+
+val get_default_yaml_str : unit -> string
+(** [get_default_yaml_str ()] returns the default Yaml configuration, as a string,
+  for this current cwd. *)
