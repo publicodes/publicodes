b34c33555a1a44fc9614243c714921900bb6b241
Willow Barraco <contact@willowbarraco.fr>
2026-04-23 09:23:00 +0200

    feat: support for the "importer" mecanism
    This implement an import feature, with a new mecanism "importer". The
    expanded rules are namespaced similarly to "avec". The imported rules
    are never re-exported, and the public is then used to allow cross-module
    references.
    
    This can take multiple forms:
    
    	# to import a module
    	importer: foo
    
    	# a more verbose form
    	importer:
    	  module: foo
    
    	# to import a module from a different package
    	importer:
    	  module: foo
    	  package: bar
    
    The "bar" package is determined using the environment variable
    PUBLICODESPATH, splitted on ":". Each part is a directory root to find
    the matching package. Not specifying a package means the module is
    looked for in the current package.
    
    All path arguments are validated, and the format is restricted to avoid
    leaving the expected directory. This also gives better hints when an
    invalid path is given.
    
    Importing modules starting with "./" will cause a relative import. The
    dot part is expanded with the current module path, meaning the imported
    module will be looked for relatively to the current module directory
    path.
    
    Similarly to the "./" module path prefix, we also support a "./" prefix
    for PUBLICODESPATH parts. When this prefix is present, we will be
    looking for the vendor package from the package directory, instead of
    pwd. This allow us to support the nested node_modules strategy. Contrary
    to hoisted or shallow strategies, nested is structured this way:
    
    	node_modules/
    	  package a/
    	    node_modules/
    	      package b/
    
    To cover all use cases, we can use "./node_modules:node_modules" as
    PUBLICODESPATH.
    
    We now track crawled module ids to detect circular imports, and
    cross-module reference. This refactorize a bit how we pass
    current_rule_name, to limit the number of argument we have to pass
    arround.
    
    The node id are now generated using also the rule name to avoid clash
    on successive imports of the same modules. Because the id does not come
    exclusively from the position anymore, we move it to the Shared module.
    
    The "config" and "init" compile sub-commands has been dropped, because
    now the config file feels obsolete, as this simplify the compiler
    API to a single folder, as a module unit.
    
    The "module" and "package" value validation has beed dependency
    inverted, so that the gathering function only checks that arguments are
     valid Fpaths. This allow the compiler to gather files from non-valid
    values, as relative paths as "./foo", or absolute paths as /foo.
    
    Change-Id: I478bbc567c4137878e3eb8adeba758a66a6a6964

--- packages/compiler/lib/utils/file.mli
+++ packages/compiler/lib/utils/file.mli
@@ -7,3 +7,35 @@ val read_file : t -> string
 
 val write_file : path:t -> content:string -> unit
 (** [write_file ~path ~content] writes the [content] to the file at [path]. *)
+
+val is_valid_import : string -> bool
+(** [is_valid_import ~path] checks that a value is a valid Publicode module or
+  package *)
+
+val relativize : string -> string -> string
+(** [relativize ~dir ~path] in case of relative path, concat the two
+  valid import path strings to build a relative module directory path.
+  Returns the path unchanged if arguments are invalid paths *)
+
+type gather_module_error =
+  | Invalid_path of string
+  | Not_found of string
+  | Is_not_directory of string
+  | Empty_directory of string
+
+val gather_module :
+  ?package:string -> string -> (string list, gather_module_error) result
+(** [publicodes_module ~package ~module] list Publicodes files in a package
+  module. *)
+
+type find_package_error =
+  | Invalid_path of string
+  | Not_found of string list
+  | Absent_env
+  | Empty_env
+  | Invalid_env of string list
+
+val find_package :
+  string option -> string -> (string, find_package_error) result
+(** [publicodes_package ~current_package ~path] finds the path to the package
+  directory. *)
