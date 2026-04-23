dcc8cf9e492585e91860cc4827b4cc7ba1c488d2
Willow Barraco <contact@willowbarraco.fr>
2026-04-23 09:23:00 +0200

    feat: import feature
    This implement an import feature, with a new mecanism "importer". This
    take a list of file path, read and parse them as if they were compiled
    directly. The expanded rules are namespaced similarly to "avec".
    
    The imported rules are never re-exported, and the public is then used
    to allow cross-module references.
    
    First we move parse_file to the parser module. This prepare the import
    feature.
    
    We now track crawled module ids to detect circular imports, and
    cross-module reference. This refactorize a bit how we pass
    current_rule_name, to limit the number of argument we have to pass
    arround.
    
    The node id are now generated using also the rule name to avoid clash
    on successive imports of the same modules. Because the id does not come
    exclusively from the position anymore, we move it to the Shared module.
    
    Change-Id: I478bbc567c4137878e3eb8adeba758a66a6a6964

--- packages/compiler/lib/parser/parse.ml
+++ packages/compiler/lib/parser/parse.ml
@@ -8,7 +8,7 @@ let authorized_keys =
   Parse_meta.reserved_meta
   @ Hashtbl.keys Parse_mechanisms.chainable_mechanisms
   @ Hashtbl.keys Parse_mechanisms.value_mechanisms
-  @ ["remplace"; "avec"; "rend non applicable"]
+  @ ["remplace"; "avec"; "rend non applicable"; "importer"]
   (* To implement *)
   @ [ "barème"
     ; "grille"
@@ -27,15 +27,22 @@ let authorized_keys =
     ; "privé"
     ; "logarithme" ]
 
-let rec parse_rule ~default_to_public ?(current_rule_name = []) (name, yaml) =
+type context =
+  { current_rule_name: string list (* the rule name currently parsed *)
+  ; files: string list (* list of parsed files paths to detect cycles *)
+  ; current_module_id: Shared.Module_id.t (* the module id genealogy *)
+  ; next_module_id: int ref (* the next module id to be assigned *) }
+
+let rec parse_rule ~default_to_public ~ctx (name, yaml) =
   let* name, pos = parse_ref name in
-  let name = current_rule_name @ name in
+  let name = ctx.current_rule_name @ name in
   let* value = Parse_value.parse_value ~error_if_undefined:false ~pos yaml in
   let default_meta = if default_to_public then [Public] else [] in
+  let module_id = Module_id ctx.current_module_id in
   let parsed_rule =
     { name= Pos.mk ~pos (Shared.Rule_name.create_exn name)
     ; value
-    ; meta= default_meta
+    ; meta= module_id :: default_meta
     ; replace= []
     ; make_not_applicable= [] }
   in
@@ -52,27 +59,95 @@ let rec parse_rule ~default_to_public ?(current_rule_name = []) (name, yaml) =
       in
       let* meta = Parse_meta.parse yaml in
       let meta = default_meta @ meta in
-      let* with_ = parse_with ~default_to_public ~current_rule_name:name yaml in
+      let* with_ =
+        parse_with ~default_to_public
+          ~ctx:{ctx with current_rule_name= name}
+          yaml
+      in
+      let* import =
+        parse_import ~default_to_public
+          ~ctx:{ctx with current_rule_name= name}
+          yaml
+      in
       let* replace = parse_replace yaml in
       let* make_not_applicable = parse_make_not_applicable yaml in
       return
-        ( { name= Pos.mk ~pos (Shared.Rule_name.create_exn name)
-          ; value
-          ; meta
-          ; replace
-          ; make_not_applicable }
-        :: with_ )
+        ( [ { name= Pos.mk ~pos (Shared.Rule_name.create_exn name)
+            ; value
+            ; meta= module_id :: meta
+            ; replace
+            ; make_not_applicable } ]
+        @ with_ @ import )
   | `A _ ->
       (* Should not happen because already checked by parse_value*)
       empty
 
-and parse_with ~default_to_public ?(current_rule_name = []) mapping =
+and parse_with ~default_to_public ~ctx mapping =
   let rules = find_value "avec" mapping in
   match rules with
   | None ->
       return []
   | Some (rules, pos) ->
-      parse_rules ~default_to_public ~pos ~current_rule_name rules
+      parse_rules ~default_to_public ~pos ~ctx rules
+
+and parse_import ~default_to_public ~ctx mapping =
+  match find_value "importer" mapping with
+  | None ->
+      return []
+  | Some (import, pos) -> (
+    match import with
+    | `A yaml -> (
+        let* values = yaml |> List.map ~f:(get_scalar ~pos) |> all_keep_logs in
+        let input_files =
+          List.map ~f:(fun ({value; _}, pos) -> (value, pos)) values
+        in
+        let circular_files =
+          List.find input_files ~f:(fun (filename, _) ->
+              List.exists ctx.files ~f:(String.equal filename) )
+        in
+        match circular_files with
+        | Some (circular, pos) ->
+            let code, message = Err.import_cycle (circular :: ctx.files) in
+            fatal_error ~pos ~code ~kind:`Syntax message
+        | None ->
+            let input_files =
+              List.map ~f:(fun (value, _) -> value) input_files
+            in
+            parse_files ~default_to_public ~ctx input_files )
+    | _ ->
+        let code, message = Err.parsing_should_be_array in
+        fatal_error ~pos ~code ~kind:`Syntax message )
+
+and parse_files ~default_to_public ~ctx input_files =
+  let module_id = !(ctx.next_module_id) in
+  ctx.next_module_id := !(ctx.next_module_id) + 1 ;
+  let+ unresolved_programs =
+    List.map input_files ~f:(fun filename ->
+        (* Read the file content *)
+        let file_content = File.read_file filename in
+        (* Parse the file content *)
+        to_yaml ~filename file_content
+        >>= parse_rules ~default_to_public
+              ~pos:(Pos.beginning_of_file filename)
+              ~ctx:
+                { ctx with
+                  files= filename :: ctx.files
+                ; current_module_id=
+                    Shared.Module_id.append ctx.current_module_id module_id } )
+    |> all_keep_logs
+  in
+  List.fold
+    ~f:(fun acc program -> Ast.merge acc program)
+    ~init:[] unresolved_programs
+
+and parse_root ~default_to_public input_files =
+  let ctx =
+    { current_rule_name= []
+    ; files= []
+    ; current_module_id= Shared.Module_id.empty
+    ; next_module_id= ref 0 }
+  in
+  parse_files ~default_to_public ~ctx input_files
 
 and parse_replace mapping =
   let replace = find_value "remplace" mapping in
@@ -92,17 +167,17 @@ and parse_make_not_applicable mapping =
         ~f:(Parse_replace.parse_make_not_applicable ~pos)
         make_not_applicable
 
-and parse_rules ~default_to_public ~pos ?(current_rule_name = []) yaml =
+and parse_rules ~default_to_public ~pos ~ctx yaml =
   match yaml with
   | `O [] ->
       let code, message =
-        if List.is_empty current_rule_name then Err.yaml_empty_file
+        if List.is_empty ctx.current_rule_name then Err.yaml_empty_file
         else Err.parsing_should_be_object
       in
       fatal_error ~code ~pos ~kind:`Syntax message
   | `O mapping ->
       let+ rules =
-        List.map ~f:(parse_rule ~default_to_public ~current_rule_name) mapping
+        List.map ~f:(parse_rule ~default_to_public ~ctx) mapping
         |> all_keep_logs
       in
       List.concat rules
@@ -112,4 +187,10 @@ and parse_rules ~default_to_public ~pos ?(current_rule_name = []) yaml =
 
 let parse ~filename ?(default_to_public = false) (yaml : yaml) : Ast.t Output.t
     =
-  parse_rules ~default_to_public ~pos:(Pos.beginning_of_file filename) yaml
+  let ctx =
+    { current_rule_name= []
+    ; files= [filename]
+    ; current_module_id= Shared.Module_id.empty
+    ; next_module_id= ref 0 }
+  in
+  parse_rules ~default_to_public ~pos:(Pos.beginning_of_file filename) ~ctx yaml
