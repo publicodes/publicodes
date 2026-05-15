171ebafebba889e68a4bfc997305d6b6863c8e35
Willow Barraco <contact@willowbarraco.fr>
2026-05-15 10:33:42 +0200

    feat: support for packages
    This expand the "importer" mecanism to support a more verbose form:
    
    exporter:
      module: foo
      package: bar # optional
    
    The "bar" package is determined from the environment variable
    PUBLICODESPATH, splitted on ":". Each part is a directory root to find
    the matching package. Not specifying a package means the module is
    looked for in the current package.
    
    Change-Id: I7cd4b07e0f6cea555200e6ea1e95d1f76a6a6964

--- packages/compiler/lib/utils/file.ml
+++ packages/compiler/lib/utils/file.ml
@@ -1,3 +1,4 @@
+open Option
 open Base
 
 type t = string
@@ -24,15 +25,67 @@ let write_file ~path ~content =
   | file ->
       Out_channel.with_open_bin file (write content)
 
-let publicodes_module module_ =
-  if not (Stdlib.Sys.file_exists module_) then None
-  else if not (Stdlib.Sys.is_directory module_) then None
+let publicodes_module ?package module_ =
+  let* path =
+    match package with
+    | None -> (
+      match Fpath.of_string module_ with
+      | Error _ ->
+          None
+      | Ok module_ ->
+          Some module_ )
+    | Some package ->
+        let package = Fpath.of_string package in
+        let module_ = Fpath.of_string module_ in
+        if Result.is_error package || Result.is_error module_ then None
+        else
+          let package = Stdlib.Result.get_ok package in
+          let module_ = Stdlib.Result.get_ok module_ in
+          Some (Fpath.append package module_)
+  in
+  let pathstr = Fpath.to_string path in
+  if not (Stdlib.Sys.file_exists pathstr) then None
+  else if not (Stdlib.Sys.is_directory pathstr) then None
   else
-    let dir = Fpath.of_string module_ |> Stdlib.Result.get_ok in
     let files =
-      Stdlib.Sys.readdir module_ |> List.of_array
-      |> List.map ~f:(Fpath.add_seg dir)
+      Stdlib.Sys.readdir pathstr |> List.of_array
+      |> List.map ~f:(Fpath.add_seg path)
       |> List.filter ~f:(Fpath.has_ext "publicodes")
       |> List.map ~f:Fpath.to_string
     in
     if List.is_empty files then None else Some files
+
+let publicodes_package path =
+  let* path =
+    match Fpath.of_string path with Error _ -> None | Ok path -> Some path
+  in
+  let* value =
+    match Stdlib.Sys.getenv_opt "PUBLICODESPATH" with
+    | None ->
+        Stdlib.Printf.eprintf "Warning: missing PUBLICODESPATH\n" ;
+        None
+    | Some value ->
+        Some value
+  in
+  let vendors =
+    String.split value ~on:':'
+    |> List.filter ~f:(function str -> not (String.equal "" str))
+    |> List.map ~f:Fpath.of_string
+  in
+  if not (List.filter vendors ~f:Result.is_error |> List.is_empty) then
+    Stdlib.Printf.eprintf "Warning: invalid PUBLICODESPATH\n" ;
+  let valid_vendors =
+    List.filter vendors ~f:Result.is_ok |> Result.all |> Stdlib.Result.get_ok
+  in
+  let directories =
+    List.map valid_vendors ~f:(function loc -> Fpath.append loc path)
+  in
+  let existing_directories =
+    List.filter directories ~f:(function dir ->
+        let dir = Fpath.to_string dir in
+        if not (Stdlib.Sys.file_exists dir) then false
+        else if not (Stdlib.Sys.is_directory dir) then false
+        else true )
+  in
+  let* directory = List.hd existing_directories in
+  Some (Fpath.to_string directory)
