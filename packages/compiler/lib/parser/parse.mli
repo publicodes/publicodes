049829e5bb93bcd31cf87e0a75b3e2629d8bba66
Willow Barraco <contact@willowbarraco.fr>
2026-06-06 09:32:00 +0200

    chore: fix Parse signatures
    Change-Id: I6ef07e3280881b1e8ab0d59d0c78584c6a6a6964

--- packages/compiler/lib/parser/parse.mli
+++ packages/compiler/lib/parser/parse.mli
@@ -1,12 +1,11 @@
+open Base
+open Utils
+
 val parse_root :
-     default_to_public:bool
-  -> module_:Base.string
-  -> Base.String.t Base.List.t
-  -> Base.string Base.list Shared.Shared_ast.rule_def Base__List.t Base.option
-     * Utils.Log.t Base__List.t
+  default_to_public:bool -> module_:string -> string list -> Ast.t Output.t
 
 val parse :
-     filename:Base.string
+     filename:string
   -> ?default_to_public:bool
   -> Yaml_parser.yaml
   -> Ast.t Yaml_parser.Output.t
