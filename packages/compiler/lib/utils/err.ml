04c326952089b824316fda58ecf8ed52db4b5fdf
Emile Rolley <emile.rolley@tuta.io>
2026-06-09 17:43:24 +0200

    fix: typo in error
    Change-Id: I698283c02306033a0615392599b2c9366a6a6964

--- packages/compiler/lib/utils/err.ml
+++ packages/compiler/lib/utils/err.ml
@@ -105,7 +105,7 @@ let invalid_meta = (Code.Parsing_invalid_meta, "meta invalide")
 
 let invalid_rule_name = (Code.Parsing_invalid_rule_name, "nom de règle invalide")
 
-let type_invalid_type = (Code.Type_invalid_type, "type invalide détécté")
+let type_invalid_type = (Code.Type_invalid_type, "type invalide détecté")
 
 let type_incoherence = (Code.Type_incoherence, "types non cohérents entre eux")
 
