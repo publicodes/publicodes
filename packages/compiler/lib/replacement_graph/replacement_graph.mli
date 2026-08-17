574ea8e69853c1e29b36c27f70671b56e5fc3815
Emile Rolley <emile.rolley@tuta.io>
2026-08-17 16:30:27 +0200

    refactor(compiler): simplify the replacement graph types
    Re-use the Shared_ast.replace type.
    
    Change-Id: I96ad84c52bfe73ee122d5bc31459819d6a6a6964

--- packages/compiler/lib/replacement_graph/replacement_graph.mli
+++ packages/compiler/lib/replacement_graph/replacement_graph.mli
@@ -1,13 +1,17 @@
 (** Dependency graph analysis. *)
 
+open Shared
+
+type replacement = Rule_graph.replacement
+
 val mk_and_checks :
-  Shared.Shared_ast.resolved -> (Rule_graph.t * Rule_graph.t) Utils.Output.t
+  Shared_ast.resolved -> (Rule_graph.t * Rule_graph.t) Utils.Output.t
 
 val find_replacements :
-     from:Rule_graph.vertex
-  -> rule:Rule_graph.vertex
+     from:Rule_name.t
+  -> rule:Rule_name.t
   -> Rule_graph.t
-  -> (Shared.Rule_name.t * Rule_graph.replace_meta Utils.Mark.pos) list
+  -> (Rule_name.t * replacement Utils.Mark.pos) list
 
 val find_transitive_replacements :
      from:Rule_graph.vertex
