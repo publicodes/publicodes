465fceab34f1f88466dd9dc22c967775469b2b77
Emile Rolley <emile.rolley@tuta.io>
2026-08-17 17:54:01 +0200

    fix(compiler): handle replacements in the dependency graph
    Replaced references were not used to build the dependency graph.
    
    NOTE: we still need to improve the error message to explicitly explain
    that the cycle warning is due to a replacement and handle indirections
    for transitive replacements.
    
    Change-Id: I8b3c970c386c3087689318801f400f916a6a6964

--- packages/compiler/lib/dependency_graph/rule_graph.mli
+++ packages/compiler/lib/dependency_graph/rule_graph.mli
@@ -112,7 +112,7 @@ open Shared
 (** {1 Graph construction} *)
 
 module Rule_vertex : sig
-  type t = Rule_name.t * Rule_context.t list [@@deriving equal, compare]
+  type t = Rule_name.t * Rule_context.stack [@@deriving equal, compare]
 
   val hash : t -> int
 end
@@ -136,8 +136,10 @@ module Oper : sig
   include Graph.Oper.S with type g = t
 end
 
-val mk : Shared_ast.resolved -> t
-(** [mk ast] creates a dependency graph from the given resolved [ast].
+val mk :
+  Shared_ast.resolved -> replacement_graph:Replacement_graph.Rule_graph.t -> t
+(** [mk ast ~replacement_graph] creates a dependency graph from the given
+    resolved [ast] and [replacement_graph].
 
     The graph is constructed based on the rules defined in the AST, where each
     rule is represented as a vertex in the graph. Edges are created between
