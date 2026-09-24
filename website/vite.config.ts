2ffc92fe6d4b07f2534fb4b670b80745864256bc
Emile Rolley <emile.rolley@tuta.io>
2026-09-24 16:18:24 +0200

    feat(website): new blog post about replaces

--- website/vite.config.ts
+++ website/vite.config.ts
@@ -7,7 +7,8 @@ export default defineConfig({
 		exclude: [
 			'svelte-codemirror-editor',
 			'codemirror',
-			'@codemirror/language-yaml' /* ... */
+			'@codemirror/language-yaml'
+			/* ... */
 		]
 	},
 	server: {
